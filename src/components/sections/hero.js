6403beafd8340f4c4609887ef7441c985348d0a2
vishal <vishal.singh2114@gmail.com>
2025-02-01 18:40:04 +0530

    updated about.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,15 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Vishal Singh</h2>;
+  const three = <h3 className="big-heading">I develop Innovative ML solutions.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a design and analysis engineer specializing in building automation and scalable ML
+        solutions. Currently, I’m focused on building aircrafts at{' '}
+        <a href="https://www.boeing.co.in/" target="_blank" rel="noreferrer">
+          Boeing India
         </a>
         .
       </p>
